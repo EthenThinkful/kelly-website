3b578f838dd5c9371d644c5d22dc2e5f2d822552
Ethen Roth <Ethenfm@gmail.com>
2022-12-12 16:20:03 -0700

    testing branch pushing

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -5,7 +5,7 @@ function Menu() {
 		// ex:
 		// <Link className="nav-link" to="/dashboard">
 		
-	<h1 className="text-3xl font-bold underline"> Hello world! </h1>
+	<h1 className="text-3xl font-bold underline"> Hello world </h1>
 	)
 }
 
