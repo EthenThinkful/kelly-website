63e368826fc6d2bbeb79e0d590cba897ac75fa00
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-12-12 15:26:05 -0800

    changed world to earth

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 function Menu() {
-	return (
-		// ex:
-		// <Link className="nav-link" to="/dashboard">
-		
-	<h1 className="text-3xl font-bold underline"> Hello world! </h1>
-	)
-}
+  return (
+    // ex:
+    // <Link className="nav-link" to="/dashboard">
 
+    <h1 className="text-3xl font-bold underline"> Hello Earth </h1>
+  );
+}
 
 export default Menu;
