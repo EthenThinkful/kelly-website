4dfeba801f0eacce90d4473c6e89c5da6f78cef3
Ethen Roth <Ethenfm@gmail.com>
2022-12-12 18:46:35 -0700

    changed logo. Need to change logo & header text. Also need to fix margins.

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -1,11 +1,67 @@
 import React from "react";
+import "./Layout.css";
+import { useState } from "react";
 
 function Menu() {
-  return (
-    // ex:
-    // <Link className="nav-link" to="/dashboard">
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-    <h1 className="text-3xl font-bold underline"> Hello Mars </h1>
+  return (
+    <div className="flex items-center justify-between border-b border-gray-400 py-8 navigation-container">
+      <a href="/">
+		<div className="resizeImage">
+        <img src="https://user-images.githubusercontent.com/104235709/207202957-591e720e-8396-4d40-a2ac-0a3a174d0c2f.png" alt="logo" />
+		</div>
+	  </a>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="flex flex-col items-center justify-between min-h-[150px]">
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/about">Gallery</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+          <li>
+            <a href="/about">Gallery</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
