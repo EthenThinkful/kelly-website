eaf2357f4293610909260cf94693fff6136b54d2
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-12-20 22:57:42 -0800

    Added title and intro blurb

---
+++ src/layout/Intro.js
@@ -0,0 +1,10 @@
+function Intro() {
+  return (
+    <h2 className="font-serif text-2xl text-center flex justify-center ">
+      Breathing new life into old spaces through creative refurbishment
+      services.
+    </h2>
+  );
+}
+
+export default Intro;
--- src/layout/ProjectRoutes.js
+++ src/layout/ProjectRoutes.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Carousel from "./Carousel";
+import Intro from "./Intro";
 //import NotFound from "./NotFound"
 
 function ProjectRoutes() {
   return (
-    <div className="container mx-auto mb-8 px-8">
-      <Routes>
-        <Route path="/" element={<Carousel />}></Route>
-      </Routes>
-    </div>
+    <>
+      <div className="container-intro mx-auto mb-8 px-8">
+        <Routes>
+          <Route path="/" element={<Intro />}></Route>
+        </Routes>
+      </div>
+      <div className="container-carousel mx-auto mb-8 px-8">
+        <Routes>
+          <Route path="/" element={<Carousel />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
